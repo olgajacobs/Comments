32a38419f373f951c5af9df7c0d9fc90d72d98d7
olgajacobs <olga.yakubtseva@gmail.com>
2023-03-31 22:37:09 +0200

    hw fixed

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ const textareaInputElement = document.getElementById("comment-input");
 const mainForm = document.querySelector(".add-form");
 const loadingPage = document.getElementById("loading_comments")
 
+// статус загрузки комментариев
+
 loadingPage.style.display = "flex";
 
+// функция даты
 
 function getDate(date) {
   const options = {
@@ -24,36 +27,107 @@ function getDate(date) {
 
 let comments = [];
 
-window.addEventListener('input', function () {
-  if (textInputElement.value === '' || textareaInputElement.value === '') {
-    buttonComment.disabled = true;
-  } else {
-    buttonComment.disabled = false;
-  }
-});
+
+// функция загрузки комментариев
 
 const fetchAndLogComments = () => {
   return fetchPromise = fetch("https://webdev-hw-api.vercel.app/api/v1/olya-jacobs/comments", {
   method: "GET"
 })
 .then((response) => {
-  return  response.json()
+  if (response.status === 500) {
+      throw new Error("Кажется сервер сломался")};
+      return  response.json()
 })
 .then((responseData) => {
   comments = responseData.comments;
-  })
-.then(() =>{
   loadingPage.style.display = "none";
   renderComments();
+  })
+.catch((error) => {
+    if (error.message === "Кажется сервер сломался") {
+      alert("Сервер упал")
+      return;
+    }
 })
 };
 
 fetchAndLogComments();
 
+// функция проверки что все поля заполнены
 
+const isValidForm = () => {
+  if (textInputElement.value === "" || textareaInputElement.value === "") {
+    buttonComment.disabled = true;
+    return false;
+  } else {
+    buttonComment.disabled = false;
+    return true;
+  }
+};
 
+// функция публикации комментария
 
-const enter = () => { 
+const postComment =() => {
+
+  const formElement =  mainForm.innerHTML; 
+      
+      fetch ("https://webdev-hw-api.vercel.app/api/v1/olya-jacobs/comments", {
+      method: "POST",
+      body: JSON.stringify({
+        date: new Date,
+        likes: 0,
+        isLiked: false,
+        name: textInputElement.value.replaceAll("<","&lt;").replaceAll(">","&gt;"),
+        text: textareaInputElement.value.replaceAll("<","&lt;").replaceAll(">","&gt;").replaceAll("QUOTE_BEGINS", "<div class='quote'>").replaceAll("QUOTE_ENDS", "</div>"),
+    
+      })
+    })
+    .then((response) => {
+      if (response.status === 400){
+        throw new Error("Имя или текст короче 3 символов");
+      } 
+      if (response.status === 500) {
+        throw new Error("Кажется сервер сломался")
+      }
+
+      const formElement =  mainForm.innerHTML; 
+      mainForm.innerHTML = `<div class="adding"> Комментарий отправляется..</div>`;
+      return fetchAndLogComments();
+      })
+    .then(() => {
+      mainForm.innerHTML = formElement;
+    })
+    .catch((error) => {
+      if (error.message === "Кажется сервер сломался") {
+        mainForm.innerHTML = formElement;
+        alert("Сервер упал")   
+      };
+      if (error.message === "Имя или текст короче 3 символов") {
+        mainForm.innerHTML = formElement;
+        alert("Имя или текст не могут быть короче 3 символов")     
+      }
+    })
+    .then(() => {
+      textInputElement.value = "";
+      textareaInputElement.value = "";
+    })
+    renderComments();
+
+}
+
+// отправка комментарий нажатием Enter
+
+mainForm.addEventListener("keyup", (action) => {
+
+  if (action.code === "Enter" && isValidForm()) {
+    postComment();
+ 
+}})
+
+ // обработчик клика при нажатии на кнопку "написать"
+
+buttonComment.addEventListener("click", () => {
 
   if (textInputElement.value === '' || textareaInputElement.value === '') {
     if (textInputElement.value === '') 
@@ -62,60 +136,22 @@ const enter = () => {
       textareaInputElement.classList.add('error');
     return;
 }
+ postComment()
 
-      else { 
-        
-        const formElement =  mainForm.innerHTML; 
-        mainForm.innerHTML = `<div class="adding"> Комментарий отправляется..</div>`;
+ textInputElement.classList.remove('error');
+ textareaInputElement.classList.remove('error');
+ renderComments();
+}) 
 
-        
-        fetch ("https://webdev-hw-api.vercel.app/api/v1/olya-jacobs/comments", {
-        method: "POST",
-        body: JSON.stringify({
-          date: new Date,
-          likes: 0,
-          isLiked: false,
-          name: textInputElement.value.replaceAll("<","&lt;").replaceAll(">","&gt;"),
-          text: textareaInputElement.value.replaceAll("<","&lt;").replaceAll(">","&gt;").replaceAll("QUOTE_BEGINS", "<div class='quote'>").replaceAll("QUOTE_ENDS", "</div>")
-        })
-      })
-      .then(() => {
-        return fetchAndLogComments();
-        })
-      .then(() => {
-        mainForm.innerHTML = formElement;
-      
-      })
-      
-    renderComments();
+// обработчик клика при нажатии на кнопку "удалить последний комментарий"
 
-   
-    textInputElement.value = "";
-    textareaInputElement.value = "";
-
-  };
- 
-}
-  
-
-
-  document.addEventListener("keyup", (e) => {
-    if (e.code === "Enter") {
-      enter();
-    }
-      });
-
-
-  buttonComment.addEventListener("click", enter) 
-
-
-  removeComment.addEventListener("click", () => {
+removeComment.addEventListener("click", () => {
     listElement.removeChild(listElement.lastElementChild);
 });
 
+// функция добавления лайка
 
-
-  const initAddLike = () => {
+const initAddLike = () => {
   
     const addLikeButtons = document.querySelectorAll(".like-button");
 
@@ -137,12 +173,12 @@ const enter = () => {
         }
 
         renderComments ();
-      });
-    } ;
+      })
+    }
   }
 
 
-
+// функция ответа на комментарий
 
 const answer =() => {
 
@@ -155,13 +191,15 @@ const answer =() => {
 
       const index = answerComment.dataset.index;
    
-      textareaInputElement.textContent = `QUOTE_BEGINS ${comments[index].name} : \n ${comments[index].text} QUOTE_ENDS`;
+      textareaInputElement.textContent = `QUOTE_BEGINS ${comments[index].author.name} : \n ${comments[index].text} QUOTE_ENDS`;
     
-    });
+    })
   }
 }
-    
-  
+
+
+// функция рендер комментариев
+
   const renderComments = () => {
 
     const listElementHtml = comments
@@ -184,9 +222,7 @@ const answer =() => {
           <button data-index="${index}" class="${comment.isLiked ? "like-button -active-like " : "like-button "}" id ="like-input"></button>
         </div>
       </div>
-    </li>`;
-
-   
+    </li>`
     })
     .join("") ;
 
@@ -197,9 +233,7 @@ const answer =() => {
 
   initAddLike();
   answer();
-};
-
-answer();
+}
 
 
 console.log("It works!");
